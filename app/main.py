72a2372a86420039819dc56cbb5251485d29794d
rk.dev <rekubota.dev@gmail.com>
2026-06-10 14:58:33 +0900

    AlembicによるDBマイグレーション管理を導入

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-
-from app.database import engine
-from app.models import Base
 from app.router.studies import router as studies_router
 
 
 app = FastAPI(title="Study Record API")
-
-
-# テーブル作成
-Base.metadata.create_all(bind=engine)
 
 app.include_router(studies_router)
